4958ff66628776ddc2f3e11429b81c8ab802ccc8
Nampy <at_ben@encs.concordia.ca>
2018-07-08 02:32:01 -0400

    added new files

---
+++ src/Dictionary.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class Dictionary {
+    ArrayList fruits = new ArrayList();
+
+    public Dictionary(){
+        // constructor to instantiate
+    }
+
+    //adding new entries to the dictionary
+    public void newEntry(){
+
+    }
+}
