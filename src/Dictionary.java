74af7a3a894088d2cb5d7b94313d262eb844b92c
Nampy <at_ben@encs.concordia.ca>
2018-07-08 02:47:04 -0400

    created a hashmap

--- src/Dictionary.java
+++ src/Dictionary.java
@@ -1,14 +1,23 @@
-import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Dictionary {
-    ArrayList fruits = new ArrayList();
+
+    // fruits with a description
+    HashMap<String, String> fruits = new HashMap<>();
 
     public Dictionary(){
         // constructor to instantiate
     }
 
     //adding new entries to the dictionary
-    public void newEntry(){
+    public void newEntry(String fruitName, String message){
+        // populate the dictionary
+        fruits.put(fruitName, message);
+    }
+
+    // look up the fruits the user wants, if it doesn't exist return an error message
+
+    public void look(String fruitName){
 
     }
 }
